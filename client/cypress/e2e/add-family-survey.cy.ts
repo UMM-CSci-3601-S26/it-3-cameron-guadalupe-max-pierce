d003c9d79a0f2b45c98067e7d1980fdd369357c9
Max Matejka <matej065@morris.umn.edu>
2026-04-02 14:51:22 -0500

    fixed survey.cy e2e files so it should pass now

--- client/cypress/e2e/add-family-survey.cy.ts
+++ client/cypress/e2e/add-family-survey.cy.ts
@@ -2,22 +2,18 @@ describe('Backpack Need Survey', () => {
   it('visits survey and submits required data', () => {
     cy.visit('/families/survey');
 
-    cy.get('[name=familyLastName]').type('Smith');
+    cy.get('[name=familyLastName]').should('be.visible').type('Smith');
     cy.get('[name=childFirstName]').type('Emma');
     cy.get('[name=school]').type('Lincoln Elementary');
+
     cy.get('[name=grade]').click();
     cy.get('mat-option').contains('4').click();
+
     cy.get('mat-radio-button[value="yes"]').click();
 
-    cy.get('form').submit();
+    cy.get('button[type="submit"]').click();
 
-    cy.on('window:alert', (str) => {
-      expect(str).to.equal('Thank you for submitting!');
-    });
-
-    cy.get('[name=familyLastName]').should('have.value', '');
-    cy.get('[name=childFirstName]').should('have.value', '');
-    cy.get('[name=school]').should('have.value', '');
-    cy.get('[name=grade]').should('contain.text', 'Grade');
+    cy.url({ timeout: 10000 }).should('include', '/families');
+    cy.contains('FAMILIES').should('be.visible');
   });
 });
