b156d6ea5a7e4713176544ec86ce58a0add0a386
Mindthemoods <prcrichards@gmail.com>
2026-04-16 12:46:13 -0500

    Add a lil more seed data

--- client/src/app/families/family_list.component.ts
+++ client/src/app/families/family_list.component.ts
@@ -7,6 +7,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
+import {MatExpansionModule} from '@angular/material/expansion';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -40,6 +41,7 @@ import { MatToolbar } from '@angular/material/toolbar';
   providers: [],
   imports: [
     MatCardModule,
+    MatExpansionModule,
     MatFormFieldModule,
     MatInputModule,
     FormsModule,
